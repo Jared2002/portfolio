2e11319991ea8ba74f9b32b7f7a480f651c8ca15
Jared Romero <jared.romeroalvirde@gmail.com>
2022-01-15 08:19:33 -0600

    Adding 2 technologies

--- src/components/tech.js
+++ src/components/tech.js
@@ -3,7 +3,6 @@ import { Fragment } from "react/cjs/react.production.min";
 
 const H2 = ({text,id}) => <h2 id={id}>{text}</h2>
 const Img = ({src, c}) => <img className={c} src={src} alt="hola"></img>
-const P = ({text, c, id}) => <p id={id} className={c}>{text}</p>
 const Button = ({text, c, onClick, id}) => <button id={id} className={c} onClick={onClick}>{text}</button>
 const Tech = () =>{
     const [textState, setTextState] = useState("Ver más");
@@ -29,7 +28,10 @@ const Tech = () =>{
           <Img c="tech-image-3" src={process.env.PUBLIC_URL + "/assets/icons/js.svg"}/>
           <Img c="tech-image-4" src={process.env.PUBLIC_URL + "/assets/icons/react.svg"}/>
         </div>
-        <P id="dark" c="pOct none" text="Por el momento estas son mis únicas tecnologías :("/>
+        <div className="pOct none tech-image-container">
+          <Img c="tech-image-1" src={process.env.PUBLIC_URL + "/assets/icons/sass.svg"}/>
+          <Img c="tech-image-2" src={process.env.PUBLIC_URL + "/assets/icons/webpack.svg"}/>
+        </div>
         <Button id="dark" text={textState} c="button more" onClick={handleClickMore}/>
       </Fragment>
     )
